cfae32a1bf199091c5ef237d1d1a9c653a75e1e0
makrohard <410733@gmail.com>
2024-12-31 05:40:07 +0100

    finalized

---
+++ img/README.md
@@ -0,0 +1,10 @@
+PIXELFLUT BOUNCER
+
+Client for pixelflut server, written on 38C3 by makrohard.  
+Creates a moving image. Can be scaled by using threading. 
+
+* Specify server + port in matrix2wall.
+* Specify resolution of pixelflood-server. Connect to server via nc and enter: "SIZE" to get the resolution
+* Start matrix2wall to start sending data
+
+
--- img2matrix.py
+++ img2matrix.py
@@ -1,8 +1,8 @@
 from PIL import Image, ImageDraw
 from io import BytesIO
 
-# uses two black + transparent images.
-# returns matrix of black pixels
+# create matrix with x/y-coords + color
+# from fully black pixel of .png images
 
 def main():
     # open images
@@ -14,7 +14,7 @@ def main():
     inner = scale_image(inner, scale_factor)
     outer = scale_image(outer, scale_factor)
 
-    # create matrix of full black pixels
+    # create matrix of fully black pixels
     color_inner = 'FFFFFF'
     color_outer = 'FF0000'
     matrix_inner, matrix_outer = byte_stream(inner, outer, color_inner, color_outer)
@@ -28,6 +28,13 @@ def main():
     matrix2image(matrix)
     return matrix
 
+# scale the image
+def scale_image(image, scale_factor):
+    width, height = image.size
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+    return image.resize((new_width, new_height))
+
 # use scaled image to create pixelmatrix
 def byte_stream(inner, outer, color_inner, color_outer):
     with BytesIO() as byte_io:
@@ -40,13 +47,6 @@ def byte_stream(inner, outer, color_inner, color_outer):
     matrix_outer = image2matrix(outer_data, color_outer)
     return matrix_inner, matrix_outer
 
-# scale an image
-def scale_image(image, scale_factor):
-    width, height = image.size
-    new_width = int(width * scale_factor)
-    new_height = int(height * scale_factor)
-    return image.resize((new_width, new_height))
-
 # write black pixels from image to pixelmatrix
 def image2matrix(image_data, color):
     img = Image.open(BytesIO(image_data)).convert('RGBA')
@@ -58,13 +58,14 @@ def image2matrix(image_data, color):
                 pixel_matrix.append((x, y, color))
     return pixel_matrix
 
+# make datapackage smaller by removing pixel
 def remove_data_points(pixel_matrix, percentage):
     if percentage <= 0.5:
         return [point for i, point in enumerate(pixel_matrix) if (i + 1) % max(1, int(1 / percentage)) != 0]
     else:
         return [point for i, point in enumerate(pixel_matrix) if (i + 1) % max(1, int(1 / (1 - percentage))) == 0]
 
-# re-convert pixelmatrix to image for debug
+# re-convert pixelmatrix to image and display the result
 def matrix2image(pixel_matrix):
     bg_color = (0, 0, 0, 255)
     width = max(pixel[0] for pixel in pixel_matrix)
@@ -74,6 +75,6 @@ def matrix2image(pixel_matrix):
     for x, y, color in pixel_matrix:
         rgb_color= tuple(int(color[i:i + 2], 16) for i in (0, 2, 4))
         draw.point((x, y), fill=rgb_color)
-    img.show()
+    img.show()  #< - - - - - comment this line to prevent image popup on start - - - -
 
 matrix = main()
